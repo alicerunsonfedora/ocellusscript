0a401a876d6fdf6f6f6afe0d5dfaaa305a12ff02
Marquis Kurt <software@marquiskurt.net>
2020-02-04 19:57:01 -0500

    [tests + .github + ocellusscript] Use pytest

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -3,36 +3,22 @@
 The following test functions are provided to test that the lexer
 works as intended for both files and regular strings.
 """
-from efficacy import OSTokenizer, OSTokenType
-from tests.utils import test, TestError
 from os import getcwd
+from efficacy import OSTokenizer
 
-@test(name="basic string tokenization")
-def tokenize_basic_string():
+def test_tokenize_basic_string():
     """Test that the lexer can tokenize a given string."""
     lexer = OSTokenizer()
     source = "example takes Integer returns Integer\nexample t = t > 6 ? t + 5 : t\n"
 
     tokens = lexer.tokenize(source)
-    if not tokens:
-        raise TestError("Expected a list of tokens but received an empty list.")
+    assert len(tokens) > 0
 
-    print(tokens)
-    # if tokens != expected:
-    #     raise TestError("Tokenized list doesn't match expected tokens.")
-
-@test(name="file tokenization")
-def tokenize_file():
+def test_tokenize_file():
     """Test that the lexer can tokenize an OcellusScript file."""
     lexer = OSTokenizer()
     source = ""
     with open(getcwd() + "/tests/main.ocls", "r") as srcfile:
         source = srcfile.read()
     tokens = lexer.tokenize(source)
-    if not tokens:
-        raise TestError("Expected a list of tokens but received an empty list.")
-
-if __name__ == "__main__":
-    print("Running lexer tests...")
-    tokenize_basic_string()
-    tokenize_file()
+    assert len(tokens) > 0
