342bfd5a8aef313adf85a1c3fc91f2c76676d38f
Marquis Kurt <software@marquiskurt.net>
2020-02-05 15:37:39 -0500

    [tests] Write file output test for CLI

---
+++ tests/test_cli.py
@@ -0,0 +1,42 @@
+"""This module contains the CLI tests for Efficacy.
+
+The following test functions are provided to test that the CLI
+application works as intended.
+"""
+
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+
+from efficacy.cli import run_cli
+import json
+import os
+
+def test_write_tokens():
+    """Test that the token writing functionality works as intended."""
+    current_tokens = []
+    expected_tokens = []
+
+    if "tmp" in os.listdir():
+        for file in os.listdir("tmp"):
+            os.remove("tmp/" + file)
+        os.rmdir("tmp")
+
+    os.mkdir("tmp")
+
+    with open("tmp/sample.ocls", "w+") as sample:
+        sample.write("example t = t > 6.0 ? t + 5.3 : t\n")
+    run_cli(["-i", "tmp/sample.ocls", "-oT", "tmp/basic.json"])
+
+    with open("tmp/basic.json", "r") as curr:
+        current_tokens = json.loads(curr.read())
+
+    for file in os.listdir("tmp"):
+        os.remove("tmp/" + file)
+    os.rmdir("tmp")
+
+    with open("tests/lexer_samples/basic.json") as expec:
+        expected_tokens = json.loads(expec.read())
+    assert current_tokens == expected_tokens
