cafd3397dd0b26f18468fb5ed0b703cc0576aa1f
Marquis Kurt <software@marquiskurt.net>
2020-03-05 20:39:14 -0500

    [compiler] Start fleshing out tokenizer

--- compiler/src/Tokenizer.kt
+++ compiler/src/Tokenizer.kt
@@ -7,11 +7,56 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+import TokenType
+import TokenizerState
+
 public class OSTokenizer(var script: String) {
 
-    // var tokens: List<Pair> = arrayListOf()
+    private var chars: Iterable<Char> = this.script.asIterable()
+    var tokenType: TokenType? = null
+    var identifier: String = ""
+    var symbol: Char = '\n'
+    var comment: String = ""
+    var docstring: String = ""
 
-    fun tokenize() {
-        var chars = this.script.asIterable()
+    /**
+     * Determine whether there are more characters to process.
+     *
+     * @return Boolean of whether chars is not empty
+     */
+    fun hasMoreChars(): Boolean {
+        return this.chars.count() > 0
     }
+
+    /**
+     * Grab the next character if it exists.
+     */
+    fun getNextChar(): Char {
+        if (!hasMoreChars()) {
+            throw NoSuchElementException("There are no more characters to process.")
+        }
+        return this.chars.first()
+    }
+
+    /**
+     * Reset the current token state.
+     */
+    fun resetToken() {
+        this.tokenType = null
+        this.identifier = ""
+        this.symbol = '\n'
+        this.comment = ""
+        this.docstring = ""
+    }
+
+    /**
+     * Generate the next token, when ready.
+     */
+    fun advance() {
+        var state = TokenizerState.START
+        var tokenType: TokenType? = null
+        var token = ""
+        var curr = this.getNextChar()
+    }
+
 }
